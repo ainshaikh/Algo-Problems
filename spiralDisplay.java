8b7253dec36e42526de53486a7504d1a5c0f8806
MOHD AIN <ainsaikhx7@gmail.com>
2022-09-19 14:05:41 +0530

    :fire:spiral display traversal matrice

---
+++ spiralDisplay.java
@@ -0,0 +1,54 @@
+import java.io.*;
+import java.util.*;
+public class spiralDisplay {
+    public static void main(String[] args) throws Exception {
+        Scanner scn = new Scanner(System.in);
+
+        int n = scn.nextInt();
+        int m = scn.nextInt();
+
+        int [][]arr = new int [n][m];
+        for(int i=0; i<arr.length; i++){
+            for(int j=0; j<arr[0].length; j++){
+                arr[i][j] = scn.nextInt();
+            }
+        }
+
+        int minr = 0;
+        int minc = 0;
+        int maxr = arr.length-1;
+        int maxc = arr[0].length-1;
+        int tne = n*m;
+        int count = 0;
+
+        while(count < tne){
+            //left
+            for(int i=minr, j=minc; i<=maxr && count < tne; i++){
+                System.out.println(arr[i][j]);
+                count++;
+            }
+            minc++;
+
+            //bottom
+            for(int i=maxr, j=minc; j<=maxc && count<tne; j++){
+                System.out.println(arr[i][j]);
+                count++;
+            }
+            maxr--;
+
+            //right
+            for(int i=maxr, j=maxc; i>=minr && count<tne; i--){
+                System.out.println(arr[i][j]);
+                count++;
+            }
+            maxc--;
+
+            //top
+            for(int i=minr, j=maxc; j>=minc && count<tne; j--){
+                System.out.println(arr[i][j]);
+                count++;
+            }
+            minr++;
+        }
+    }
+}
