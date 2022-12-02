4c99e57602e285e83170d3255f452889edc087b7
MOHD AIN <ainsaikhx7@gmail.com>
2022-12-02 11:39:59 +0530

    :tada:Find Peak Element of an Array

---
+++ Array/FindaPeekElement.java
@@ -0,0 +1,37 @@
+// package Algo.Array;5
+import java.util.*;
+
+
+
+
+
+public class FindaPeekElement {
+
+     static int findPeak (int arr [], int n) {
+        if (n == 1) {
+            return 0;
+        }
+        if(arr[0] >= arr[1]){
+            return 0;
+        }
+        if(arr[n-1]>= arr[n-2]){
+            return n-1;
+        }
+    
+        for(int i=1; i<n-1; i++){
+            if(arr[i]>=arr[i-1] && arr[i]>=arr[i+1])
+            return i;
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int arr[] = new int [n];
+        for(int i=0; i<arr.length; i++){
+            arr[i] = scn.nextInt();
+        }
+        System.out.println("Index is " + findPeak(arr, n) );
+    }
+}
